e64c61fa7d54a5191f2aa7dfd9832a4b0bfae519
Jason Tucker <jtucker@gmail.com>
2015-12-01 13:39:15 -0500

    Working version with canistream function defined. Will output  a list of streams for the provided movie.

--- stream_check.fsx
+++ stream_check.fsx
@@ -1,11 +1,13 @@
 #r "packages/FSharp.Data/lib/net40/FSharp.Data.dll"
 open FSharp.Data
+open FSharp.Data.JsonExtensions
 
 // Patterns http://regexr.com/3c936
 
 type MovieSearch = JsonProvider<"samples/search.json">
 type MovieDetails = JsonProvider<"samples/details.json">
-
+ 
+ 
 let httpHeaders = [HttpRequestHeaders.UserAgent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36"]
 let searchApiUrl = "http://www.canistream.it/services/search"
 let queryApiUrl  = "http://www.canistream.it/services/query"
@@ -23,7 +25,19 @@ let getMovieDetails movieId =
         query = ["movieId", movieId; "attributes", "1"; "mediaType", "streaming"], 
         headers = httpHeaders) |> 
     MovieDetails.Parse
+
+let streamble (movie:MovieDetails.Root) =
+    match movie.JsonValue.Properties with
+    | x when Array.isEmpty x -> printfn "no streams found"
+    | x -> 
+        Array.iter (fun (name, value) -> 
+          let friendlyName = value?friendlyName.AsString()
+          printfn "Stream: %s" friendlyName
+        ) x
     
-searchMovie("harry and the hendersons") |>
-Seq.map(fun movie -> getMovieDetails movie.Id) |> 
-Seq.map(fun movie -> printfn "Amazon: %s" movie.AmazonPrimeInstantVideo.Url)
+let canistream moviename = 
+    searchMovie(moviename) 
+    |> Array.tryHead 
+    |> function 
+       | Some x -> streamble(getMovieDetails(x.Id))
+       | None -> printfn "not found"
